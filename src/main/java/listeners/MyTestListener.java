660b1fa50ded053a5e145ac01985c12aaae270dc
VictoriiaFeshchenko <vikafeshch@gmail.com>
2023-07-28 20:32:54 +0300

    added 26 lesson

---
+++ src/main/java/listeners/MyTestListener.java
@@ -0,0 +1,47 @@
+package listeners;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class MyTestListener implements ITestListener {
+    @Override
+    public void onTestStart(ITestResult result) {
+        System.out.println("start "+result.getMethod().getMethodName()+" ****");
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        System.out.println("++++++WIN+++++++");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        System.out.println("======Fall=======");
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        ITestListener.super.onTestSkipped(result);
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+        ITestListener.super.onTestFailedButWithinSuccessPercentage(result);
+    }
+
+    @Override
+    public void onTestFailedWithTimeout(ITestResult result) {
+        ITestListener.super.onTestFailedWithTimeout(result);
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+        ITestListener.super.onStart(context);
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        ITestListener.super.onFinish(context);
+    }
+}
